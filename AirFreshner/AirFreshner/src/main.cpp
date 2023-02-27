8067eb5a9cf883b4f5c032af211b90e5b43a0edc
FabianRutten <94969946+FabianRutten@users.noreply.github.com>
2023-02-27 13:55:17 +0100

    sensor declares

--- AirFreshner/AirFreshner/src/main.cpp
+++ AirFreshner/AirFreshner/src/main.cpp
@@ -9,7 +9,23 @@ unsigned long myTime;
 // Display
 LiquidCrystal lcdScreen(12, 11, 5, 4, 3, 2);
 
+// Distance Sensor, which is sonar based
+NewPing sonar(7,6,200);
 
+// Light sensor, LDR
+#define LDR A0;
+
+// Button bus, which is used for 3 buttons on analogPin 2
+#define BUTTON_BUS A2;
+
+// Bus for many digital signals on the bus
+#define ONE_WIRE_BUS 8
+
+// OneWire protocol to make use of the one_wire_bus
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Temperature sensor from Dallas on the oneWire bus
+DallasTemperature tempSensor = (&oneWire);
 
 
 void updateTime(){
@@ -24,7 +40,7 @@ void setup() {
 void loop() {
   // the running timer is constantly updated
   updateTime();
-  
+
 }
 
 
