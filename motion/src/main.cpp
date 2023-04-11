07106aeebca562cf6dae1fe0c535d02f08f4f06b
Thijs Olijerhoek <t.g.a.olijerhoek@students.uu.nl>
2023-04-11 14:12:31 +0200

    added assignment 2 and some motion

--- motion/src/main.cpp
+++ motion/src/main.cpp
@@ -1,19 +1,56 @@
 #include <Arduino.h>
-#include <I2Cdev.h>
-#include <Wire.h>
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+struct MyData {
+  byte X;
+  byte Y;
+};
+
+MyData data;
+
+void setup()
+{
+  Serial.begin(115200);
   Wire.begin();
-  Serial.begin(38400);
+  mpu.initialize();
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN,LOW);
+void loop()
+{
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  data.X = map(ax, -17000, 17000, 0, 255 ); // X axis data
+  data.Y = map(ay, -17000, 17000, 0, 255);  // Y axis data
+  //delay(500);
+  Serial.print("Axis X = ");
+  Serial.print(data.X);
+  Serial.print("  ");
+  Serial.print("Axis Y = ");
+  Serial.println(data.Y);
+  if (data.Y < 80) { //gesture : down 
+    Serial.print("true");
+    //Serial.print("gesture 1");
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  if (data.Y > 145) {//gesture : up
+    Serial.print("false");
+    digitalWrite(LED_BUILTIN, LOW);
+    //Serial.print("gesture 2");
+  }
+  if (data.X > 155) {//gesture : left
+    //Serial.print("gesture 3");
+  }
+  if (data.X < 80) {//gesture : right
+    //Serial.print("gesture 4");
+  }
+  if (data.X > 100 && data.X < 170 && data.Y > 80 && data.Y < 130) { //gesture : little bit down
+    //Serial.print("gesture 5");
+  }
   delay(1000);
 }
